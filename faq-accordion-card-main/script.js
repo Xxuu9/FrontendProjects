772dc356328a60213c2a16cb867f2ec1c14e3749
Sophie <tengxxu@gmail.com>
2021-07-25 13:13:50 -0400

    Add js file

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -3,9 +3,17 @@ let qas = document.querySelectorAll('.question_answer_accordion');
 
 qas.forEach(qa => qa.addEventListener('click',event => {
         let currentEvent = event.currentTarget;
+        if (currentEvent.querySelector('.answer').classList.contains('show')) {
+            currentEvent.querySelector('.answer').classList.remove('show');
+        }
+        else{
+            document.querySelectorAll('.answer').forEach(answer => {
+                answer.classList.remove('show');
+            })
 
-        document.querySelectorAll('.answer').forEach(answer => {
-            answer.classList.remove('show');
-        })
-        currentEvent.querySelector('.answer').classList.add('show');
+            currentEvent.querySelector('.answer').classList.add('show');
+
+        }
+
+
     }))
